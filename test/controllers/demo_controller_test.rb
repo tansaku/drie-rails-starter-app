fc27675e5150cd7fdc82cb418a3fcbe3a70cd854
Kalbir Sohi <kalbir@gmail.com>
2017-07-27 15:32:52 +0100

    remove the demo-praxis controller

--- test/controllers/demo_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class DemoControllerTest < ActionDispatch::IntegrationTest
-  test "should get praxis" do
-    get demo_praxis_url
-    assert_response :success
-  end
-
-end
